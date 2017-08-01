a11c80deddb48b331719b111c0be61630f60c372
jmarti <jmarti@theinit.com>
2017-08-01 17:57:35 +0200

    refactor to a parser object

--- app/gol-turn.js
+++ app/gol-turn.js
@@ -1,42 +1,4 @@
-import {split, tap, pipe, map, forEach, trim} from 'ramda';
-import {checkLength} from './lib/arrays';
-import {throwIf} from './lib/errors';
-import {stringBefore, stringAfter, stringContainsOnly} from './lib/strings';
-
-const INVALID_INPUT_MSG = "Invalid input. Please, use something like this: \n3 2\n.*\n*.\n..";
-const throwInvalidMsgIf = throwIf(INVALID_INPUT_MSG);
-
-const checkNumber = (number) => throwInvalidMsgIf(isNaN(number));
-
-const getDimensions = pipe(
-    stringBefore("\n"),
-    split(" "),
-    tap(map(checkNumber)),
-    map(parseInt),
-    tap(checkLength(2, INVALID_INPUT_MSG))
-);
-
-const checkDimensions = (dimensions) => (rows) => {
-    checkLength(dimensions[0], INVALID_INPUT_MSG)(rows);
-    rows.forEach(checkLength(dimensions[1], INVALID_INPUT_MSG));
-};
-
-const checkCharacters = (rows) => {
-    const checkValidRow = (row) => {
-        const validateRow = stringContainsOnly(".*");
-        throwInvalidMsgIf(!validateRow(row));
-    };
-
-    rows.forEach(checkValidRow);
-}
-
-const getDash = (dimensions) => pipe(
-    stringAfter("\n"),
-    trim,
-    split("\n"),
-    tap(checkDimensions(dimensions)),
-    tap(checkCharacters)
-);
+import {getDimensions, getDash} from './gol-parser'
 
 export const golTurn = (input) => {
 
